5f04b0e1ba968a1093247e348c1302c634816b47
JoonShik <jshong0907@gmail.com>
2022-07-10 22:10:41 +0900

    copy all file to container

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ from schemas import InputSchema
 def handler(event, context):
     input = InputSchema(**event)
     return input.json()
+
+
+if __name__ == '__main__':
+    print(InputSchema(**{
+        "request_id": "10",
+        "message": "message",
+        "request_date": "2022-07-13"
+    }).json())
