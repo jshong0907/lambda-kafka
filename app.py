d8d77cb6525e460c086b2e45d46204b0b17afc6d
JoonShik <jshong0907@gmail.com>
2022-07-10 22:12:26 +0900

    remove pycache

--- app.py
+++ app.py
@@ -4,11 +4,3 @@ from schemas import InputSchema
 def handler(event, context):
     input = InputSchema(**event)
     return input.json()
-
-
-if __name__ == '__main__':
-    print(InputSchema(**{
-        "request_id": "10",
-        "message": "message",
-        "request_date": "2022-07-13"
-    }).json())
